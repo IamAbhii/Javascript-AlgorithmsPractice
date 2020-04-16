fbbd6989f2e02b3bf8d543974b834376271851f5
abhi <abrahmbhatt03@gmai.com>
2020-04-16 14:03:01 -0500

    myLanguageScore

--- algoJs.js
+++ algoJs.js
@@ -15,3 +15,24 @@ function sumEvenNumbers(input) {
 }
 
 console.log(sumEvenNumbers([4, 3, 1, 2, 5, 10, 6, 7, 9, 8]));
+
+// You are given a dictionary/hash/object containing some languages and your test results in the given languages. Return the list of languages where your test score is at least 60, in descending order of the results.
+
+// Note: the scores will always be unique (so no duplicate values)
+
+// Examples
+// {"Java": 10, "Ruby": 80, "Python": 65}    -->  ["Ruby", "Python"]
+// {"Hindi": 60, "Dutch" : 93, "Greek": 71}  -->  ["Dutch", "Greek", "Hindi"]
+// {"C++": 50, "ASM": 10, "Haskell": 20}     -->  []
+
+function myLanguages(results) {
+  const final = [];
+
+  for (let key in results) {
+    if (results[key] >= 60) final.push(key);
+  }
+
+  return final.sort((a, b) => results[b] - results[a]);
+}
+
+console.log(myLanguages({ Hindi: 60, Dutch: 93, Greek: 71 }));
