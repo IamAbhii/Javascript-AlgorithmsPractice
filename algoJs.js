c544cdf76806518961f592ca17e6991bcdec5487
abhi <abrahmbhatt03@gmai.com>
2020-04-18 13:16:58 -0500

    newAlgoRelatedToArray

--- algoJs.js
+++ algoJs.js
@@ -36,3 +36,54 @@ function myLanguages(results) {
 }
 
 console.log(myLanguages({ Hindi: 60, Dutch: 93, Greek: 71 }));
+
+// Task
+// Given a positive integer n, calculate the following sum:
+
+// n + n/2 + n/4 + n/8 + ...
+// All elements of the sum are the results of integer division.
+
+// Example
+// 25  =>  25 + 12 + 6 + 3 + 1 = 47
+
+function halvingSum(n) {
+  let number = n;
+  let numberArray = [];
+  while (number >= 1) {
+    numberArray.push(number);
+    number = Math.floor(number / 2);
+  }
+
+  return numberArray.reduce((a, c) => a + c, 0);
+}
+
+console.log(halvingSum(25));
+
+// Create a function named divisors/Divisors that takes an integer n > 1 and returns an array with all of the integer's divisors(except for 1 and the number itself), from smallest to largest. If the number is prime return the string '(integer) is prime' (null in C#) (use Either String a in Haskell and Result<Vec<u32>, String> in Rust).
+
+// Example:
+// divisors(12); // should return [2,3,4,6]
+// divisors(25); // should return [5]
+// divisors(13); // should return "13 is prime"
+function divisors(integer) {
+  //make an array to store value
+  //get the modulo result and push it to the array
+  //check for emty array.
+  //if it is emplty return string written as a prime
+
+  const resultArray = [];
+
+  for (let index = 2; index <= Math.floor(integer / 2); index++) {
+    if (integer % index === 0) {
+      resultArray.push(index);
+    }
+  }
+
+  if (resultArray.length === 0) {
+    return `${integer} is prime`;
+  }
+
+  return resultArray;
+}
+
+console.log(divisors(12));
